7e0edd9b2cfa84d5a6005901c0aaded5b9fcd403
mh <mh@immerda.ch>
2010-08-17 11:31:52 +0200

    use correct define

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,5 +1,5 @@
 class mod_perl::centos inherits mod_perl::base {
-  apache::conf::global{'perl.conf':
+  apache::config::global{'perl.conf':
     source => "modules/mod_perl/apache/${operatingsystem}/perl.conf",
     require => Package['mod_perl'],
   }

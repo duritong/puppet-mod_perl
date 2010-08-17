fb6657277428bd82db3b53e99647d684107cc58e
mh <mh@immerda.ch>
2010-08-17 11:21:18 +0200

    fix typo

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,5 +1,5 @@
 class mod_perl::centos inherits mod_perl::base {
-  apache::conf::gloabl{'perl.conf':
+  apache::conf::global{'perl.conf':
     source => "modules/mod_perl/apache/${operatingsystem}/perl.conf",
     require => Package['mod_perl'],
   }
